bde155c6046e83a0401fb692f67e73f6aca1a6fa
Kapoor <naman.kapoor@capgemini.com>
2021-06-06 15:10:45 +0530

    test 3

---
+++ gtu/src/main/java/rfdgrty/gtu/GtuApplication.java
@@ -0,0 +1,13 @@
+package rfdgrty.gtu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtuApplication.class, args);
+	}
+
+}
---
+++ gtu/src/test/java/rfdgrty/gtu/GtuApplicationTests.java
@@ -0,0 +1,13 @@
+package rfdgrty.gtu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
